ab9869bbbb6b1f5792edf062f09f71877e62f40d
Ambrala <ambralaju@gmail.com>
2020-08-20 01:28:44 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,19 +1,12 @@
-let rad = 100; // Width of the shape
-let xpos, ypos; // Starting position of shape
-
-let xspeed = 3.8; // Speed of the shape
-let yspeed = 3.2; // Speed of the shape
-
-let xdirection = 1; // Left or Right
-let ydirection = 1; // Top to Bottom
+let angle1 = 0;
+let angle2 = 0;
+let scalar = 200;
 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 
 }
-
-
 
 
 function setup() {
@@ -33,15 +26,11 @@ function setup() {
 	rable = new Image();
     rable.src = "lib/day03.png";
 	
-noStroke();
-  frameRate(80);
-  ellipseMode(RADIUS);
-  // Set the starting position of the shape
-  xpos = windowWidth/2;
-  ypos = windowHeight/2;
+	me = new Image();
+    me.src = "lib/me01.png";
+	
+//   meMode(CENTER);
 }
-
-
 
 
 
@@ -49,23 +38,30 @@ noStroke();
 function draw() {
 	background('#99CCFF');
 	
- xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
+  let ang1 = radians(angle1);
+  let ang2 = radians(angle2);
 
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
-  if (xpos > width - rad || xpos < rad) {
-    xdirection *= -1;
-  }
-  if (ypos > height - rad || ypos < rad) {
-    ydirection *= -1;
-  }
+  let x1 = width / 3.7 + scalar * cos(ang1);
+  let x2 = width / 2 + scalar * cos(ang2);
 
-  // Draw the shape
-  drawingContext.drawImage(img,xpos, ypos, rad, rad);
-  drawingContext.drawImage(day,xpos/3, ypos/2, rad, rad);
-    drawingContext.drawImage(memo,xpos/4, ypos, rad, rad);
-	  drawingContext.drawImage(rable,xpos/2, ypos/4, rad, rad);
+  let y1 = height / 2 + scalar * sin(ang1);
+  let y2 = height / 2 + scalar * sin(ang2);
+
+  fill(0);
+  drawingContext.drawImage(me,width * 0.35, height * 0.25, 300, 300);
+
+  fill(0);
+  drawingContext.drawImage(img, x1, height * 0.25 - 190, scalar, scalar);
+  drawingContext.drawImage(day,x2, height * 0.25 + 275, scalar, scalar);
+
+  fill(0);
+  drawingContext.drawImage(memo,width * 0.35 - 148, y1, scalar, scalar);
+  drawingContext.drawImage(rable,width * 0.35 + 220, y2, scalar, scalar);
+
+  angle1 += 2;
+  angle2 += 3;
+
+
 }
 
 
